3206d4959b92eef5916b9ca32178eef4592cc85e
Dainis <gatesby@protonmail.com>
2025-06-12 17:55:08 +0300

    fixed it not working

--- main.py
+++ main.py
@@ -18,22 +18,28 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if client.user.mention in message.content.split():
-        words = message.content.split()
-        randomWord = random.choice(words)
-        randomWordAlt = random.choice(words)
-        lines = [
-            f"Did you know that {randomWord} massively funds zionist lobbies?",
-            f"Yes, {randomWord} is redacted in JFK files.",
-            f"***Readers added context they thought people might want to know***\n{randomWord} is {randomWordAlt}.",
-            f"https://tenor.com/view/true-truth-nuke-super-truth-nova-truth-meme-gif-16889273424352737553",
-            f"https://tenor.com/view/walter-white-false-nuke-walter-white-false-breaking-bad-false-breaking-bad-false-nuke-gif-11851145952566757188",
-            f"https://tenor.com/view/america-usa-election-maba-flip-america-blue-gif-17801180",
-            f"starboard bait",
-            f"{randomWord} is fake and gay",
-            f"@Grok is this {randomWord}?"
-        ]
-        await message.channel.send(random.choice(lines))
+    try:
+        if client.user.mention in message.content.split():
+            words = message.content.split()
+            randomWord = random.choice(words)
+            randomWordAlt = random.choice(words)
+
+            lines = [
+                f"Did you know that {randomWord} massively funds zionist lobbies?",
+                f"Yes, {randomWord} is redacted in JFK files.",
+                f"***Readers added context they thought people might want to know***\n{randomWord} is {randomWordAlt}.",
+                f"https://tenor.com/view/true-truth-nuke-super-truth-nova-truth-meme-gif-16889273424352737553",
+                f"https://tenor.com/view/walter-white-false-nuke-walter-white-false-breaking-bad-false-breaking-bad-false-nuke-gif-11851145952566757188",
+                f"https://tenor.com/view/america-usa-election-maba-flip-america-blue-gif-17801180",
+                f"starboard bait",
+                f"{randomWord} is fake and gay",
+                f"@Grok is this {randomWord}?"
+            ]
+
+            response = random.choice(lines)
+            await message.channel.send(response)
+    except Exception as e:
+        print(e)
     
 client.run(TOKEN)
 
